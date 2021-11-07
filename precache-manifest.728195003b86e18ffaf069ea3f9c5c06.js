714287abaee3024f94d203b6be8b788208296a5f
Zhao Bo <tqnwhz@qq.com>
2021-11-07 12:11:13 +0800

    Updates

---
+++ precache-manifest.728195003b86e18ffaf069ea3f9c5c06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b963300f41c8ba6b24e192ac46e9486e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec5372afafd692dfcc5",
+    "url": "/static/css/main.8b03490b.chunk.css"
+  },
+  {
+    "revision": "f8cbb435b1266c918b70",
+    "url": "/static/js/2.646b8152.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.646b8152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec5372afafd692dfcc5",
+    "url": "/static/js/main.64310e24.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "097f30bd8d792f4c69bd0503979ebf75",
+    "url": "/static/media/tqnwhz.097f30bd.jpg"
+  }
+]);
